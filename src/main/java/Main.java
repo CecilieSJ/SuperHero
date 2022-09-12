1aaf79928be70dc860e606e571ff146ae34682f4
CecilieSJ <ceci749q@stud.kea.dk>
2022-09-12 21:49:11 +0200

    menu påbegyndt men med manglende loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,24 +10,26 @@ public class Main {
         //Database database = new Database();
         //database.dataBase();
 
+        System.out.println("Velkommen til SuperHelte Universet \n1. Opret Superhelt \n9. Afslut");
+        int menu = scan.nextInt();
+        if(1==1){
+            System.out.println("Venligst indtast navnet på din superhelt!");
+            String heroName = scan.next();
 
-        System.out.println("Venligst indtast navnet på din superhelt!");
-        String heroName = scan.next();
+            System.out.println("Skriv hvilken superpower din superhelt har");
+            String superPower = scan.next();
 
-        System.out.println("Skriv hvilken superpower din superhelt har");
-        String superPower = scan.next();
+            System.out.println("Skriv hvilken styrke de besider");
+            String strenght = scan.next();
 
-        System.out.println("Skriv hvilken styrke de besider");
-        String strenght = scan.next();
+            System.out.println("Skriv hvilke år karrakteren blev lavet");
+            int creationYear = scan.nextInt();
 
-        System.out.println("Skriv hvilke år karrakteren blev lavet");
-        int creationYear = scan.nextInt();
+            System.out.println("Din superhelt er: " + heroName + "\nSuperpower: " + superPower + "\nStyrke :" + strenght + "\nKarrakterens udgivelses år: " + creationYear);
+        } else if (9==9) {
+            System.out.println("Afsluttet");
 
-        System.out.println("Din superhelt er: " + heroName + "\nSuperpower: " + superPower + "\nStyrke :" + strenght + "\n33Karrakterens udgivelses år: " + creationYear);
-
-
-
-
+        }
 
 
     }
